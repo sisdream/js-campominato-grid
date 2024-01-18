7990c071e69baae718f649d6976895efe66dfcb3
sisdream <sissipascale2@gmail.com>
2024-01-18 15:55:11 +0100

    feat: function grid

--- js/script.js
+++ js/script.js
@@ -4,9 +4,28 @@ const gridCont = document.getElementById("grid");
 const easyLevel = document.getElementById("easy");
 const mediumLevel = document.getElementById("medium");
 const hardLevel = document.getElementById("hard");
+const playButton = document.getElementById("play");
 let points = 0;
+generateGride(gridCont);
 
-console.log(gridCont);
-console.log(easyLevel);
-console.log(mediumLevel);
-console.log(hardLevel);
+playButton.addEventListener('click', function(){
+    generateGride(gridCont);
+});
+
+// LIVELLO EASY
+easyLevel.addEventListener("click", () => createGrid(100));
+// LIVELLO MEDIUM
+mediumLevel.addEventListener("click", () => createGrid(81));
+// LIVELLO HARD
+hardLevel.addEventListener("click", () => createGrid(49));
+
+// Funzione x griglia
+
+function generateGride(container){
+    gridCont.innerHTML = '';
+
+    for(let i = 0; i < 46; i++){
+        const squere = generateSquare();
+        container.append(squere);
+    }
+}
