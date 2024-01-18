81b33bfa0c573454ac955ab5d806649842521cd3
sisdream <sissipascale2@gmail.com>
2024-01-18 15:13:37 +0100

    feat: const js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// CREARE LE COSTANTI
+
+const gridCont = document.getElementById("grid");
+const easyLevel = document.getElementById("easy");
+const mediumLevel = document.getElementById("medium");
+const hardLevel = document.getElementById("hard");
+let points = 0;
+
+console.log(gridCont);
+console.log(easyLevel);
+console.log(mediumLevel);
+console.log(hardLevel);
