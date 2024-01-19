2e5a8ae3c6dea52c15effb143268cc1b15cba242
sisdream <sissipascale2@gmail.com>
2024-01-19 15:37:44 +0100

    fix: numero random

--- js/script.js
+++ js/script.js
@@ -8,37 +8,38 @@ const playButton = document.getElementById("play");
 let points = 0;
 generateGride(gridCont);
 
+
 playButton.addEventListener('click', function(){
     generateGride(gridCont);
 });
 
-// livello easy
-easyLevel.addEventListener("click", () => generateGride(100));
-// livello hard
-mediumLevel.addEventListener("click", () => generateGride(88));
-// livello hard
-hardLevel.addEventListener("click", () => generateGride(64));
 
-
-// Funzione x griglia
+// FUNZIONE PER CREARE LA GRIGLIA
 
 function generateGride(container){
     gridCont.innerHTML = '';
 
-    for(let i = 0; i < 100; i++){
-        const squere = generateSquare();
+    for(let i = 1; i < 100 + 1; i++){
+        const squere = generateSquare(i);
         container.append(squere);
-    };
+    }
 }
 
-//  funzione x cella
+//  FUNZIONE PER CREARE CELLA
 
-function generateSquare(){
+function generateSquare(text){
     const square = document.createElement(`div`);
     square.classList.add(`square`);
+    square.innerText = text;
     square.addEventListener('click', function(){
         this.classList.toggle('safe');
     });
     return square;
 }
+
+// METODO CHE GENERA UN NUMERO RANDOM
+
+function numeroRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
